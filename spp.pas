5414ac0476e686d6c500bdb03e8bfbe89c7513cd
garbagememo <66831242+garbagememo@users.noreply.github.com>
2022-07-07 00:02:22 +0900

    loop（非再帰）サポート

--- spp.pas
+++ spp.pas
@@ -1,4 +1,4 @@
-﻿program smallpt;
+program smallpt;
 {$MODE objfpc}{$H+}
 {$INLINE ON}
 {$modeswitch advancedrecords}
@@ -9,90 +9,107 @@ const
   eps=1e-4;
   INF=1e20;
   DefaultSamples=16;
+  M_2PI=2*pi;
 
 
 type 
-  ModelClass=CLASS
+  ModelClass=class
     p,e,c:VecRecord;// position. emission,color
     refl:RefType;
     isLight:boolean;
     constructor Create(p_,e_,c_:VecRecord;refl_:RefType);
     function intersect(const r:RayRecord):real;virtual;abstract;
     function GetNorm(x:VecRecord):VecRecord;virtual;abstract;
-  END;
+  end;
 
-  SphereClass=CLASS(ModelClass)
+  SphereClass=class(ModelClass)
     rad:real;       //radius
     rad2:real;
     constructor Create(rad_:real;p_,e_,c_:VecRecord;refl_:RefType);
     function intersect(const r:RayRecord):real;override;
     function GetNorm(x:VecRecord):VecRecord;override;
-  END;
+  end;
 
-  RectClass=CLASS(ModelClass)
+  RectClass=class(ModelClass)
     H1,H2,V1,V2:Real;
     RA:RectAxisType;
     constructor Create(RA_:RectAxisType;H1_,H2_,V1_,V2_:real;p_,e_,c_:VecRecord;refl_:RefType);
     function intersect(const r:RayRecord):real;override;
     function GetNorm(x:VecRecord):VecRecord;override;
-  END;
+  end;
 
 
   RectAngleClass=class(ModelClass)
     RAary:array[0..5] of RectClass;
     HitID:integer;
     constructor Create(p1,p2,e_,c_:VecRecord;refl_:RefType);
-  function intersect(const r:RayRecord):real;override;
-  function GetNorm(x:VecRecord):VecRecord;override;
+    function intersect(const r:RayRecord):real;override;
+    function GetNorm(x:VecRecord):VecRecord;override;
   end;
 
 
-  CameraRecord=RECORD
+  CameraRecord=record
     o,d,cx,cy : VecRecord;
     dist      : real;
-    w,h       : INTEGER;
+    w,h       : integer;
     ratio     : real;
-    samples   : INTEGER;
-    PROCEDURE Setup(o_,d_: VecRecord;w_,h_:INTEGER;ratio_,dist_:real);
-    PROCEDURE SetSamples(sam :INTEGER);
-    FUNCTION Ray(x,y,sx,sy : INTEGER):RayRecord;
-  END;
+    samples   : integer;
+    procedure Setup(o_,d_: VecRecord;w_,h_:integer;ratio_,dist_:real);
+    procedure SetSamples(sam :integer);
+    function Ray(x,y,sx,sy : integer):RayRecord;
+  end;
 
-  PROCEDURE CameraRecord.Setup(o_,d_:VecRecord;w_,h_:INTEGER;ratio_,dist_:real);
-  BEGIN
+  SceneClass=class
+    mdl:TList;
+    cam:CameraRecord;
+    constructor Create(mdl_:TList;cam_:CameraRecord);
+  end;
+  
+  TRenderThreadClass=Class
+     function radiance(r:RayRecord;depth:integer):VecRecord;virtual;
+  end;
+  TLoopRenderThreadClass=class(TRenderThreadClass)
+     function radiance(r:RayRecord;depth:integer):VecRecord;override;
+  end;
+
+  constructor SceneClass.Create(mdl_:TList;cam_:CameraRecord);
+  begin
+    mdl:=mdl_;cam:=cam_;
+  end;
+  
+  procedure CameraRecord.Setup(o_,d_:VecRecord;w_,h_:integer;ratio_,dist_:real);
+  begin
     ratio:=ratio_;dist:=dist_;w:=w_;h:=h_;
     o:=o_;d:=VecNorm(d_);
     cx:=CreateVec(ratio*w_/h_,0,0);
     cy:=VecNorm(cx/d_)*ratio;
     samples:=DefaultSamples;
-  END;
+  end;
 
-  PROCEDURE CameraRecord.SetSamples(sam :INTEGER );
-  BEGIN
+  procedure CameraRecord.SetSamples(sam :integer );
+  begin
     samples:=sam;
-  END;
+  end;
 
-  FUNCTION CameraRecord.Ray(x,y,sx,sy:INTEGER):RayRecord;
-  VAR
+  function CameraRecord.Ray(x,y,sx,sy:integer):RayRecord;
+  var
     r1,r2,dx,dy:real;
     td:VecRecord;
-  BEGIN
+  begin
     r1:=2*random;
-    IF r1<1 THEN dx:=sqrt(r1)-1 ELSE dx:=1-sqrt(2-r1);
+    if r1<1 then dx:=sqrt(r1)-1 else dx:=1-sqrt(2-r1);
     r2:=2*random;
-    IF (r2 < 1) THEN dy := sqrt(r2)-1 ELSE dy := 1-sqrt(2-r2);
+    if (r2 < 1) then dy := sqrt(r2)-1 else dy := 1-sqrt(2-r2);
     td:= cy*(((sy + 0.5 + dy)/2 + (h-y-1))/h - 0.5)+cx*(((sx + 0.5 + dx)/2 + x)/w - 0.5)+d;
     td:=VecNorm(td);
     result.o:= td*dist+ o;
     result.d := td;
-  END;
-
-
+  end;
   
   constructor ModelClass.Create(p_,e_,c_:VecRecord;refl_:RefType);
   begin
-    p:=p_;e:=e_;c:=c_;refl:=refl_;IF VecSQR(e)>0 THEN isLight:=TRUE ELSE isLight:=FALSE;
-  END;
+    p:=p_;e:=e_;c:=c_;refl:=refl_;if VecSQR(e)>0 then isLight:=TRUE else isLight:=false;
+  end;
   constructor SphereClass.Create(rad_:real;p_,e_,c_:VecRecord;refl_:RefType);
   begin
     rad:=rad_;rad2:=rad*rad; inherited create(p_,e_,c_,refl_);
@@ -105,63 +122,63 @@ type
     op:=p-r.o;
     t:=eps;b:=op*r.d;
     det:=b*b-op*op+rad*rad;
-    IF det<0 THEN 
+    if det<0 then 
       result:=INF
-    ELSE BEGIN
+    else begin
       det:=sqrt(det); t:=b-det;
-      IF t>eps then 
-	result:=t
-      ELSE BEGIN
-	t:=b+det;
-	if t>eps then 
+      if t>eps then 
+         result:=t
+      else begin
+         t:=b+det;
+         if t>eps then 
           result:=t
-	else
+         else
           result:=INF;
-      END;
-    END;
+      end;
+    end;
   end;
   function SphereClass.GetNorm(x:VecRecord):VecRecord;
-  BEGIN
+  begin
     result:=VecNorm(x-p)
-  END;
+  end;
 
   constructor RectClass.Create(RA_:RectAxisType;H1_,H2_,V1_,V2_:real;p_,e_,c_:VecRecord;refl_:RefType);
-  BEGIN
+  begin
     RA:=RA_;H1:=H1_;H2:=H2_;V1:=V1_;V2:=V2_;inherited create(p_,e_,c_,refl_);
-  END;
+  end;
   function RectClass.intersect(const r:RayRecord):real;
   var
     t:real;
     pt:VecRecord;
-  BEGIN
+  begin
     (**光線と平行に近い場合の処理が必要だが・・・**)
-    case RA OF
+    case RA of
       xy:begin
-	   result:=INF;
-	   if abs(r.d.z)<eps THEN exit;
-	   t:=(p.z-r.o.z)/r.d.z;
-	   if t<eps then exit;//result is INF
-	   pt:=r.o+r.d*t;
-	   IF (pt.x<H2) and (pt.x>H1) and (pt.y<V2)and (pt.y>V1) THEN result:=t;
-	 end;(*xy*)
+            result:=INF;
+            if abs(r.d.z)<eps then exit;
+            t:=(p.z-r.o.z)/r.d.z;
+            if t<eps then exit;//result is INF
+            pt:=r.o+r.d*t;
+            if (pt.x<H2) and (pt.x>H1) and (pt.y<V2)and (pt.y>V1) then result:=t;
+          end;(*xy*)
       xz:begin
-	   result:=INF;
-	   if abs(r.d.y)<eps THEN exit;
-	   t:=(p.y-r.o.y)/r.d.y;
-	   if t<eps then exit;//result is INF
-	   pt:=r.o+r.d*t;
-	   IF (pt.x<H2) and (pt.x>H1) and (pt.z<V2)and (pt.z>V1) THEN result:=t;
-	 end;(*xz*)
+            result:=INF;
+            if abs(r.d.y)<eps then exit;
+            t:=(p.y-r.o.y)/r.d.y;
+            if t<eps then exit;//result is INF
+            pt:=r.o+r.d*t;
+            if (pt.x<H2) and (pt.x>H1) and (pt.z<V2)and (pt.z>V1) then result:=t;
+          end;(*xz*)
       yz:begin
-	   result:=INF;
-	   if abs(r.d.y)<eps THEN exit;
-	   t:=(p.x-r.o.x)/r.d.x;
-	   if t<eps then exit;//result is INF
-	   pt:=r.o+r.d*t;
-	   IF (pt.y<H2) and (pt.y>H1) and (pt.z<V2)and (pt.z>V1) THEN result:=t;
-	 end;(*yz*)
-    END;(*case*)
-  END;
+            result:=INF;
+            if abs(r.d.y)<eps then exit;
+            t:=(p.x-r.o.x)/r.d.x;
+            if t<eps then exit;//result is INF
+            pt:=r.o+r.d*t;
+            if (pt.y<H2) and (pt.y>H1) and (pt.z<V2)and (pt.z>V1) then result:=t;
+          end;(*yz*)
+    end;(*case*)
+  end;
 
   function RectClass.GetNorm(x:VecRecord):VecRecord;
   begin
@@ -194,21 +211,23 @@ begin
   t:=INF;HitID:=-1;
   for i:=0 to 5 do begin
     d:=RAary[i].intersect(r);
-    if d<t THEN BEGIN
+    if d<t then begin
       t:=d;
       HitID:=i;
-    END;
+    end;
   end;
   result:=t;
 end;
 
 function RectAngleClass.GetNorm(x:VecRecord):VecRecord;
-BEGIN
+begin
   result:=RAary[HitID].GetNorm(x);
-END;
+end;
 
 var
   mdl:TList;
+  cam:CameraRecord;
+
 procedure InitScene;
 begin
   mdl:=TList.Create;
@@ -218,53 +237,52 @@ begin
   mdl.add( sphereClass.Create(1e5, CreateVec(50,40.8,-1e5+170),  ZeroVec,CreateVec(0,0,0),      DIFF) );//Front
   mdl.add( sphereClass.Create(1e5, CreateVec(50, 1e5, 81.6),     ZeroVec,CreateVec(0.75,0.75,0.75),DIFF) );//Bottomm
   mdl.add( sphereClass.Create(1e5, CreateVec(50,-1e5+81.6,81.6), ZeroVec,CreateVec(0.75,0.75,0.75),DIFF) );//Top
-  mdl.add( sphereClass.Create(16.5,CreateVec(27,16.5,47),        ZeroVec,CreateVec(1,1,1)*0.999, SPEC) );//Mirror
+//  mdl.add( sphereClass.Create(16.5,CreateVec(27,16.5,47),        ZeroVec,CreateVec(1,1,1)*0.999, SPEC) );//Mirror
   mdl.add( sphereClass.Create(16.5,CreateVec(73,16.5,88),        ZeroVec,CreateVec(1,1,1)*0.999, REFR) );//Glass
   mdl.add( sphereClass.Create(600, CreateVec(50,681.6-0.27,81.6),CreateVec(12,12,12),    ZeroVec,DIFF) );//Ligth
 //  mdl.add( RectClass.Create(XY,20,80,40,79,CreateVec(50,55,80), zeroVec,  CreateVec(0.25,0.75,0.25),DIFF) );
-  mdl.add( RectAngleClass.Create(CreateVec(20,40,80),CreateVec(80,70,45),zeroVec,  CreateVec(0.25,0.75,0.25),DIFF) );
-
+  mdl.add( RectAngleClass.Create(CreateVec(10,0,30),CreateVec(40,40,65),zeroVec,  CreateVec(0.66,0.99,0.66),SPEC) );
 end;
 
 function intersect(const r:RayRecord;var t:real; var id:integer):boolean;
 var 
-  n,d:real;
+  d:real;
   i:integer;
 begin
   t:=INF;
   for i:=0 to mdl.count-1 do begin
     d:=ModelClass(mdl[i]).intersect(r);
-    if d<t THEN BEGIN
+    if d<t then begin
       t:=d;
       id:=i;
-    END;
+    end;
   end;
   result:=(t<inf);
-END;
+end;
 
-function radiance(const r:RayRecord;depth:integer):VecRecord;
+function TRenderThreadClass.radiance( r:RayRecord;depth:integer):VecRecord;
 var
   id:integer;
   obj:ModelClass;
-  x,n,f,nl,u,v,w,d:VecRecord;
-  p,r1,r2,r2s,t,pr:real;
+  x,n,f,nl,d:VecRecord;
+  p,t:real;
   into:boolean;
   RefRay:RayRecord;
   nc,nt,nnt,ddn,cos2t,q,a,b,c,R0,Re,RP,Tr,TP:real;
   tDir:VecRecord;
 begin
   id:=0;depth:=depth+1;
-  if intersect(r,t,id)=FALSE then begin
+  if intersect(r,t,id)=false then begin
     result:=ZeroVec;exit;
   end;
   obj:=ModelClass(mdl[id]);
   x:=r.o+r.d*t; n:=obj.GetNorm(x); f:=obj.c;
-  IF VecDot(n,r.d)<0 THEN nl:=n else nl:=n*-1;
-  IF (f.x>f.y)and(f.x>f.z) THEN
+  if VecDot(n,r.d)<0 then nl:=n else nl:=n*-1;
+  if (f.x>f.y)and(f.x>f.z) then
     p:=f.x
-  ELSE IF f.y>f.z THEN 
+  else if f.y>f.z then 
     p:=f.y
-  ELSE
+  else
     p:=f.z;
    if (depth>5) then begin
     if random<p then 
@@ -274,22 +292,15 @@ begin
       exit;
     end;
   end;
-  CASE obj.refl OF
-    DIFF:BEGIN
-      r1:=2*PI*random;r2:=random;r2s:=sqrt(r2);
-      w:=nl;
-      IF abs(w.x)>0.1 THEN
-        u:=VecNorm(CreateVec(0,1,0)/w) 
-      ELSE
-        u:=VecNorm(CreateVec(1,0,0)/w) ;
-      v:=w/u;
-      d := VecNorm(u*cos(r1)*r2s + v*sin(r1)*r2s + w*sqrt(1-r2));
+  case obj.refl of
+    DIFF:begin
+      d:=VecSphereRef(nl);
       result:=obj.e+VecMul(f,radiance(CreateRay(x,d),depth) );
-    END;(*DIFF*)
-    SPEC:BEGIN
+    end;(*DIFF*)
+    SPEC:begin
       result:=obj.e+VecMul(f,(radiance(CreateRay(x,r.d-n*2*(n*r.d) ),depth)));
-    END;(*SPEC*)
-    REFR:BEGIN
+    end;(*SPEC*)
+    REFR:begin
       RefRay:=CreateRay(x,r.d-n*2*(n*r.d) );
       into:= (n*nl>0);
       nc:=1;nt:=1.5; if into then nnt:=nc/nt else nnt:=nt/nc; ddn:=r.d*nl; 
@@ -300,28 +311,115 @@ begin
       end;
       if into then q:=1 else q:=-1;
       tdir := VecNorm(r.d*nnt - n*(q*(ddn*nnt+sqrt(cos2t))));
-      IF into then Q:=-ddn else Q:=tdir*n;
+      if into then Q:=-ddn else Q:=tdir*n;
       a:=nt-nc; b:=nt+nc; R0:=a*a/(b*b); c := 1-Q;
       Re:=R0+(1-R0)*c*c*c*c*c;Tr:=1-Re;P:=0.25+0.5*Re;RP:=Re/P;TP:=Tr/(1-P);
-      IF depth>2 THEN BEGIN
-        IF random<p then // 反射
+      if depth>2 then begin
+        if random<p then // 反射
           result:=obj.e+VecMul(f,radiance(RefRay,depth)*RP)
-        ELSE //屈折
+        else //屈折
           result:=obj.e+VecMul(f,radiance(CreateRay(x,tdir),depth)*TP);
-      END
-      ELSE BEGIN// 屈折と反射の両方を追跡
+      end
+      else begin// 屈折と反射の両方を追跡
         result:=obj.e+VecMul(f,radiance(RefRay,depth)*Re+radiance(CreateRay(x,tdir),depth)*Tr);
-      END;
-    END;(*REFR*)
-  END;(*CASE*)
+      end;
+    end;(*REFR*)
+  end;(*CASE*)
 end;
 
-
+function TLoopRenderThreadClass.Radiance(r:RayRecord;depth:INTEGER):VecRecord;
 VAR
-  x,y,sx,sy,i,s: INTEGER;
-  w,h,samps,height    : INTEGER;
+  id:INTEGER;
+  obj:ModelClass;
+  x,n,f,nl,d:VecRecord;
+  p,t,nrd:real;
+  into:BOOLEAN;
+  RefRay:RayRecord;
+  nc,nt,nnt,ddn,cos2t,q,a,b,c,R0,Re,RP,Tr,TP:real;
+  tDir:VecRecord;
+  tv:VecRecord;
+  cl,cf:VecRecord;
+BEGIN
+//writeln(' DebugY=',DebugY,' DebugX=',DebugX);
+  depth:=0;
+  id:=0;cl:=ZeroVec;cf:=CreateVec(1,1,1);
+  WHILE (TRUE) DO BEGIN
+    Inc(depth);
+    IF intersect(r,t,id)=FALSE THEN BEGIN
+      result:=cl;
+      EXIT;
+    END;
+    obj:=ModelClass(mdl[id]);
+    x:=r.o+r.d*t; n:=obj.GetNorm(x); f:=obj.c;
+    nrd:=n*r.d;
+    IF nrd<0 THEN nl:=n ELSE nl:=n*-1;
+    IF (f.x>f.y)AND(f.x>f.z) THEN
+      p:=f.x
+    ELSE IF f.y>f.z THEN
+      p:=f.y
+    ELSE
+      p:=f.z;
+    cl:=cl+VecMul(cf,obj.e);
+    IF (Depth > 5) OR (p = 0) THEN BEGIN
+       //p=0は要するに発光体に撃ちあたる場合＝発光体は色がぜろだから
+      IF (random < p) THEN BEGIN
+        f:= f / p;
+      END
+      ELSE BEGIN
+        Result := cl;
+        EXIT;
+      END;
+    END;
+    cf:=VecMul(cf,f);
+    CASE obj.refl OF
+      DIFF:BEGIN
+        d:=VecSphereRef(nl);
+        r:=CreateRay(x,d);
+      END;(*DIFF*)
+      SPEC:BEGIN
+        tv:=n*2*nrd ;tv:=r.d-tv;
+        r:=CreateRay(x,tv);
+      END;(*SPEC*)
+      REFR:BEGIN
+        tv:=n*2*nrd ;tv:=r.d-tv;
+        RefRay:=CreateRay(x,tv);
+        into:= (n*nl>0);
+        nc:=1;nt:=1.5; IF into THEN nnt:=nc/nt ELSE nnt:=nt/nc; ddn:=r.d*nl;
+        cos2t:=1-nnt*nnt*(1-ddn*ddn);
+        IF cos2t<0 THEN BEGIN   // Total internal reflection
+          cl:=cl+VecMul(cf,obj.e);
+          r:=RefRay;
+          continue;
+        END;
+        IF into THEN q:=1 ELSE q:=-1;
+        tdir := VecNorm(r.d*nnt - n*(q*(ddn*nnt+sqrt(cos2t))));
+        IF into THEN Q:=-ddn ELSE Q:=tdir*n;
+        a:=nt-nc; b:=nt+nc; R0:=a*a/(b*b); c := 1-Q;
+        Re:=R0+(1-R0)*c*c*c*c*c;Tr:=1-Re;P:=0.25+0.5*Re;RP:=Re/P;TP:=Tr/(1-P);
+        IF random<p THEN BEGIN// 反射
+          cf:=cf*RP;
+          cl:=cl+VecMul(cf,obj.e);
+          r:=RefRay;
+        END
+        ELSE BEGIN//屈折
+          cf:=cf*TP;
+          cl:=cl+VecMul(cf,obj.e);
+          r:=CreateRay(x,tdir);
+        END
+      END;(*REFR*)
+    END;(*CASE*)
+  END;(*WHILE LOOP *)
+END;
+
+
+
+
+
+var
+  x,y,sx,sy,i,s: integer;
+  w,h,samps,height    : integer;
   temp       : VecRecord;
-  cam:CameraRecord;
+
   tColor,r : VecRecord;
 
   BMPClass:BMPIOClass;
@@ -331,36 +429,39 @@ VAR
   c:char;
    T1,T2:TDateTime;
    HH,MM,SS,MS:WORD;
-
-BEGIN
+   Rt:TRenderThreadClass;
+begin
   randomize;
   FN:='temp.bmp';
   w:=640 ;h:=480;  samps := 16;
+  Rt:=TLoopRenderThreadClass.Create;
   c:=#0;
   repeat
-    c:=getopt('o:s:w:');
-
+    c:=getopt('o:s:w:a:');
     case c of
-      'o' : BEGIN
+      'a':begin
+          Rt:=TRenderThreadClass.Create;
+      end;
+      'o' : begin
          ArgFN:=OptArg;
-         IF ArgFN<>'' THEN FN:=ArgFN;
+         if ArgFN<>'' then FN:=ArgFN;
          writeln ('Output FileName =',FN);
-      END;
-      's' : BEGIN
+      end;
+      's' : begin
         ArgInt:=StrToInt(OptArg);
         samps:=ArgInt;
         writeln('samples =',ArgInt);
-      END;
-      'w' : BEGIN
+      end;
+      'w' : begin
          ArgInt:=StrToInt(OptArg);
          w:=ArgInt;h:=w *3 div 4;
          writeln('w=',w,' ,h=',h);
-      END;
-      '?',':' : BEGIN
+      end;
+      '?',':' : begin
          writeln(' -o [finename] output filename');
          writeln(' -s [samps] sampling count');
          writeln(' -w [width] screen width pixel');
-      END;
+      end;
     end; { case }
   until c=endofoptions;
   height:=h;
@@ -370,35 +471,43 @@ BEGIN
 
  
   T1:=Time;
-  WRITELN ('The time is : ',TimeToStr(Time));
+  writeln ('The time is : ',TimeToStr(Time));
 
   Cam.Setup(CreateVec(50,52,295.6),CreateVec(0,-0.042612,-1),w,h,0.5135,140);
 
-  FOR y := 0 TO h-1 DO BEGIN
-    IF y mod 10 =0 THEN WRITELN('y=',y);
-    FOR x := 0 TO w - 1 DO BEGIN
+  for y := 0 to h-1 do begin
+    if y mod 10 =0 then writeln('y=',y);
+    for x := 0 to w - 1 do begin
       r:=CreateVec(0, 0, 0);
       tColor:=ZeroVec;
-      FOR sy := 0 TO 1 DO BEGIN
-        FOR sx := 0 TO 1 DO BEGIN
-          FOR s := 0 TO samps - 1 DO BEGIN
-            temp:=Radiance(Cam.Ray(x,y,sx,sy), 0);
+      for sy := 0 to 1 do begin
+        for sx := 0 to 1 do begin
+          for s := 0 to samps - 1 do begin
+            temp:=RT.Radiance(Cam.Ray(x,y,sx,sy), 0);
             temp:= temp/ samps;
             r:= r+temp;
-          END;(*samps*)
+          end;(*samps*)
           temp:= ClampVector(r)* 0.25;
           tColor:=tColor+ temp;
           r:=CreateVec(0, 0, 0);
-        END;(*sx*)
-      END;(*sy*)
+        end;(*sx*)
+      end;(*sy*)
       vColor:=ColToRGB(tColor);
       BMPClass.SetPixel(x,height-y,vColor);
-    END;(* for x *)
-  END;(*for y*)
+    end;(* for x *)
+  end;(*for y*)
 
   T2:=Time-T1;
   DecodeTime(T2,HH,MM,SS,MS);
-  WRITELN ('The time is : ',HH,'h:',MM,'min:',SS,'sec');
+  writeln ('The time is : ',HH,'h:',MM,'min:',SS,'sec');
 
   BMPClass.WriteBMPFile(FN);
-END.
+end.
+
+{
+検証が必要な点として
+・thread外にデータ持って大丈夫？
+がある。最終にはそっちに持ってく?
+mdl,camとintersection関数が必要だが・・・
+
+}
